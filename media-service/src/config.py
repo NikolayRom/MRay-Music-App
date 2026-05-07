12581352b3c123eae47669ad6c8cb736a4e81bd5
ntfray <rayofdetermination@mail.ru>
2026-05-07 21:54:01 +0300

    Add default and max count of tracks in get response from get_all_tracks

--- media-service/src/config.py
+++ media-service/src/config.py
@@ -10,6 +10,9 @@ class Settings(BaseSettings):
 
     POSTGRES_URL: str
 
+    TRACK_MAX_GET_SIZE: int
+    TRACK_DEFAULT_GET_SIZE: int
+
     model_config = SettingsConfigDict(env_file='../.env', extra='ignore')
 
 settings = Settings()
