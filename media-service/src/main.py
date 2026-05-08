7060e90c85a8efaed34f61910175350487213e95
ntfray <rayofdetermination@mail.ru>
2026-05-08 23:02:36 +0300

    Include new router for artists and albums

--- media-service/src/main.py
+++ media-service/src/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-from src.tracks.router import router
+from src.tracks.router import router as track_router
+from src.artists.router import router as artist_router
+from src.albums.router import router as album_router
 from src.database import lifespan
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(router=router)
+app.include_router(router=track_router)
+app.include_router(router=artist_router)
+app.include_router(router=album_router)
