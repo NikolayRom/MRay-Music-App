dc156dd418ada4ac75182ac4f609ecb696ca7021
ntfray <rayofdetermination@mail.ru>
2026-05-08 23:07:39 +0300

    Add some stripped schemas for all objects to display and avoid import cycle

---
+++ media-service/src/schemas/common.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field, ConfigDict, computed_field
+from pydantic.json_schema import SkipJsonSchema
+from typing import Optional, List, Annotated
+from datetime import datetime, timedelta
+
+class ArtistShortRead(BaseModel):
+    id: int
+    name: str
+    image_key: Optional[str] = None
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class AlbumShortRead(BaseModel):
+    id: int
+    name: str
+    image_key: Optional[str] = None
+    artist_id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class TrackShortRead(BaseModel):
+    id: int
+    title: str
+    image_key: Optional[str] = None
+    genre: List[str]
+    duration: Annotated[timedelta, Field(exclude=True), SkipJsonSchema()]
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+    @computed_field
+    @property
+    def duration_seconds(self) -> int:
+        return int(self.duration.total_seconds())
