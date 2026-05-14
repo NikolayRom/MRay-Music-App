227329e490208cbf5ac415a89d1569e6d7da6a75
ntfray <rayofdetermination@mail.ru>
2026-05-15 00:10:54 +0300

    Add some env vars for core service (minio, etc)

--- core-service/src/config.py
+++ core-service/src/config.py
@@ -2,6 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     
+    MINIO_URL: str
+    MINIO_ROOT_USER: str
+    MINIO_ROOT_PASSWORD: str
+    MINIO_BUCKET_NAME_CORE: str
+    MINIO_MAX_FILE_SIZE: int
+
     POSTGRES_URL_CORE: str
     MAX_GET_SIZE: int
     DEFAULT_GET_SIZE: int
@@ -11,6 +17,13 @@ class Settings(BaseSettings):
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int
     JWT_REFRESH_TOKEN_EXPIRE_DAYS: int
     INACTIVE_REFRESH_TOKEN_LIFETIME_DAYS: int
+
+    SUPERUSER_USERNAME: str
+    SUPERUSER_EMAIL: str
+    SUPERUSER_PASSWORD: str
+    SUPERUSER_AUTO_CREATE: bool
+
+    USER_HISTORY_LIFETIME_DAYS: int
 
     model_config = SettingsConfigDict(env_file='../.env', extra='ignore')
 
