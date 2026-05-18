ff55ac2b91d8daedc2a1f32be3acf440d507ad3f
ntfray <rayofdetermination@mail.ru>
2026-05-18 16:44:23 +0300

    Add middleware cors for all services for frontend

--- core-service/src/main.py
+++ core-service/src/main.py
@@ -7,10 +7,25 @@ from src.users.router import router as users_router
 from src.history.router import router as history_router
 from src.likes.router import router as likes_router
 from src.playlists.router import router as playlists_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='Music Streaming App (core service)',
     lifespan=lifespan
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"]
 )
 
 app.include_router(router=auth_router)
--- media-service/src/main.py
+++ media-service/src/main.py
@@ -3,10 +3,26 @@ from src.tracks.router import router as track_router
 from src.artists.router import router as artist_router
 from src.albums.router import router as album_router
 from src.common.lifespan import lifespan
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='Music Streaming App (media service)',
     lifespan=lifespan
+)
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+    expose_headers=["Content-Range", "Accept-Ranges", "Content-Length"]
 )
 
 app.include_router(router=track_router)
