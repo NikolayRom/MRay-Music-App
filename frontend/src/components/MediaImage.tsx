116646939b7f4a0c885ed8d6f2c148384217c591
ntfray <rayofdetermination@mail.ru>
2026-05-25 00:13:49 +0300

    Add new logic for MediaImage for core-assets bucket

--- frontend/src/components/MediaImage.tsx
+++ frontend/src/components/MediaImage.tsx
@@ -1,27 +1,31 @@
-import { Music, User, Disc } from 'lucide-react';
+import { Music, User, Disc, ListMusic } from 'lucide-react';
 
 interface Props {
   imageKey?: string | null;
-  type: 'track' | 'artist' | 'album';
+  type: 'track' | 'artist' | 'album' | 'playlist' | 'user'; // Добавили типы
   className?: string;
 }
 
 export const MediaImage = ({ imageKey, type, className }: Props) => {
+  // Автоматически выбираем бакет
+  const bucket = (type === 'playlist' || type === 'user') ? 'core-assets' : 'media-assets';
+
   if (imageKey) {
     return (
       <img 
-        src={`http://localhost:9000/media-assets/${imageKey}`} 
+        src={`http://localhost:9000/${bucket}/${imageKey}`} 
         className={`${className} object-cover`}
         alt="cover"
       />
     );
   }
 
-  // Заглушки
   const icons = {
     track: <Music size="40%" className="text-zinc-600" />,
     artist: <User size="40%" className="text-zinc-600" />,
     album: <Disc size="40%" className="text-zinc-600" />,
+    playlist: <ListMusic size="40%" className="text-zinc-600" />,
+    user: <User size="40%" className="text-zinc-600" />,
   };
 
   return (
